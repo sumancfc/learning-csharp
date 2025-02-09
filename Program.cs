280f89334c4b5830a0afc9587b9f6d7dc179ce8b
sumancfc <sumanstha999@gmail.com>
2025-02-09 11:19:50 +0100

    Learning C'

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace learningCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hello World: Write a program that prints "Hello, World!" to the console.
+            Console.WriteLine("Hello, World!");
+
+            /*Name and Age: Ask the user for their name and age,
+             then print a message like "Hello, [name]! You are [age] years old."*/
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            Console.Write("How old are you? ");
+            string age = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! You are {age} years old.");
+        }
+    }
+}
