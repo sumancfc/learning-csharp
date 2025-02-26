aaf0b67be1240fb18d6da6c2b6d39be79fde5c5c
Suman Shrestha <sumanstha999@gmail.com>
2025-02-26 23:18:03 +0100

    Added DVD

--- LibraryManagementSystem/Book.cs
+++ LibraryManagementSystem/Book.cs
@@ -15,3 +15,20 @@ public class Book : LibraryItem
         return $"Book: Title: {Title}, Author: {Author}, ISBN: {ISBN}, Genre: {Genre}";
     }
 }
+
+// 2. Inheritance: DVD inherits from LibraryItem
+public class DVD : LibraryItem
+{
+    public int Duration { get; set; } // Duration in minutes
+
+    public DVD(string title, string author, string isbn, int publicationYear, int duration)
+            : base(title, author, isbn, publicationYear)
+    {
+        Duration = duration;
+    }
+
+    public override string GetDetails()
+    {
+        return $"DVD: Title: {Title}, Author: {Author}, ISBN: {ISBN}, Duration: {Duration} min";
+    }
+}
